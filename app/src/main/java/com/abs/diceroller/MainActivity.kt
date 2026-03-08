357945470dc890b777c6d56c9d4ffc4360c17e84
AbdoATorky <abdalsalamtorky@gmail.com>
2026-03-08 21:17:53 +0200

    Add roll button and dice logic to MainActivity

--- app/src/main/java/com/abs/diceroller/MainActivity.kt
+++ app/src/main/java/com/abs/diceroller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.abs.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val image: ImageView = findViewById(R.id.dice_iv)
+        val  btn: Button = findViewById(R.id.roll_btn)
+
+        btn.setOnClickListener {
+            val randomNom = (1..6).random()
+            when(randomNom){
+                1 -> image.setImageResource(R.drawable.dice_1)
+                2 -> image.setImageResource(R.drawable.dice_2)
+                3 -> image.setImageResource(R.drawable.dice_3)
+                4 -> image.setImageResource(R.drawable.dice_4)
+                5 -> image.setImageResource(R.drawable.dice_5)
+                6 -> image.setImageResource(R.drawable.dice_6)
+            }
+        }
     }
+
 }
