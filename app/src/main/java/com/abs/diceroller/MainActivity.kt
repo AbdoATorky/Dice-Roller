ef76d1d81b1434fcec96e4d04ebd55bda000cb80
AbdoATorky <abdalsalamtorky@gmail.com>
2026-03-08 22:26:48 +0200

    Add dice rolling animation and delay final result visibility

--- app/src/main/java/com/abs/diceroller/MainActivity.kt
+++ app/src/main/java/com/abs/diceroller/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.abs.diceroller
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,26 +24,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val image1: ImageView = findViewById(R.id.dice_iv1)
+        val image1 = findViewById<ImageView>(R.id.dice_iv1)
         val image2: ImageView = findViewById(R.id.dice_iv2)
         val  btn: Button = findViewById(R.id.roll_btn)
 
         btn.setOnClickListener {
-            image1.setImageResource(rollDice())
-            image2.setImageResource(rollDice())
+            // 1. Set the animation drawable as the source for both ImageViews
+            image1.setImageResource(R.drawable.animation_file)
+            image2.setImageResource(R.drawable.animation_file)
+            // 2. Get and start the animation for the first die
+            val diceAnimation = image1.drawable as AnimationDrawable
+            diceAnimation.start()
+            // 3. Get and start the animation for the second die
+            val diceAnimation2 = image2.drawable as AnimationDrawable
+            diceAnimation2.start()
+            // 4. Use a Handler to delay showing the final result.
+            Handler(Looper.getMainLooper()).postDelayed({
+                image1.setImageResource(rollDice())
+                image2.setImageResource(rollDice())
+            }, 600)
         }
     }
 
     fun rollDice(): Int {
         val randomNom = (1..6).random()
-        val pic= when(randomNom){
+        return when(randomNom){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_1
+            else -> R.drawable.dice_6
         }
-        return pic
     }
 }
